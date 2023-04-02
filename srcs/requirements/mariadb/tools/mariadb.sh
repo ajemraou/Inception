1b3df284457c1f9dc6cc5b175fcc8d54ad5c648c
Ayoub Jemraoui <ajemraou@e1r11p4.1337.ma>
2023-04-02 02:33:02 +0000

    done

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -2,11 +2,12 @@
 
 echo "CREATE DATABASE $DATABASE;" > file
 echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> file
-echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;" >> file
+echo "GRANT ALL PRIVILEGES ON $DATABASE.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;" >> file
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;" >> file
 echo "FLUSH PRIVILEGES;" >> file
 
 service mysql start
-<file mysql -u root
+<file mysql
 rm file
 service mysql stop
 
